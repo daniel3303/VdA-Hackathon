72e254cf555b61e2376f120790715f9c9ab903d2
Daniel Oliveira <daniel-oliveira-11@hotmail.com>
2019-03-02 11:07:46 +0000

    Basic chat bot

---
+++ main.py
@@ -0,0 +1,42 @@
+from rasa_nlu.model import Interpreter
+import json
+from rasa_core.agent import Agent
+from rasa_core.interpreter import RasaNLUInterpreter
+import time
+
+
+
+def format_agent_message(message):
+    if(len(message) > 0):
+        return message[0]["text"]
+    else:
+        return "Desculpa, não entendi..."
+
+def main():
+    interpreter = RasaNLUInterpreter('models/current/glosa')
+    print("Bem-vindo ao Glosa")
+    agent = Agent.load('models/dialogue', interpreter=interpreter)
+
+    interpreter2 = Interpreter.load("models/current/glosa")
+
+
+
+
+    while True:
+        time.sleep(0.3)
+        print("Tu: ", end=" ")
+        message = input()
+        if message == 'sair':
+            break
+        response = agent.handle_text(message)
+        result = interpreter2.parse(message)
+        print(json.dumps(result, indent=2))
+
+        print("Glosa: ", end=" ")
+        print(format_agent_message(response))
+
+
+
+
+if __name__ == "__main__":
+    main()
