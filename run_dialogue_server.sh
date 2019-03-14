dd332efab0c7a22d20328da729d79e428cc9aba4
Daniel Oliveira <daniel-oliveira-11@hotmail.com>
2019-03-14 00:30:56 +0000

    Added cors support

--- run_dialogue_server.sh
+++ run_dialogue_server.sh
@@ -1,1 +1,1 @@
-python3 -m rasa_core.run --enable_api -d models/dialogue --endpoints config/endpoints.yml -u models/current/glosa --port 5000 --credentials config/credentials.yml
+python3 -m rasa_core.run --enable_api -d models/dialogue --endpoints config/endpoints.yml -u models/current/glosa --port 5000 --credentials config/credentials.yml --cors "*"
